c366ea6ebddf871e08ca10311cdfb3e03a6daf10
Blaine Miller <blaine141@gmail.com>
2019-11-18 17:06:56 -0500

    Fixing vscode setup again

--- setup_scripts/setup_vscode.sh
+++ setup_scripts/setup_vscode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir ../../.vscode
+mkdir -p ../../.vscode
 cp ../vscode/extensions.json ../../.vscode/
 cp ../vscode/tasks.json ../../.vscode/
 cp ../vscode/keybindings.json ~/.config/Code/User
