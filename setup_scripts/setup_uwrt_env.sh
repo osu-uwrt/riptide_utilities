48847223937e10168f6e38f5671178e295898c1b
blaine141 <blaine141@gmail.com>
2019-11-17 19:25:14 -0800

    Change build to catkin build

--- setup_scripts/setup_uwrt_env.sh
+++ setup_scripts/setup_uwrt_env.sh
@@ -9,7 +9,7 @@ sudo ~/osu-uwrt/riptide_software/src/riptide_hardware/scripts/add_rule
 
 # Compile Code
 cd ~/osu-uwrt/riptide_software
-catkin_make
+catkin build
 
 echo "If no errors occurred during compilation, then everything was setup correctly"
 echo "Please reboot your computer for final changes to take effect"
