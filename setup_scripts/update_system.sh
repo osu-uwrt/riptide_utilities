f223ad04c65f4989d8f21a9fd639d3f0a8414ba4
Blaine Miller <blaine141@gmail.com>
2019-12-24 01:08:16 -0500

    Attempted fix

--- setup_scripts/update_system.sh
+++ setup_scripts/update_system.sh
@@ -19,17 +19,17 @@ if [ -z "$ROS_DISTRO" ]; then
     fi
 fi
 
-. install_ros.sh
+./install_ros.sh
 source /opt/ros/$ROS_DISTRO/setup.bash
-. install_rosdeps.sh
+./install_rosdeps.sh
 source /opt/ros/$ROS_DISTRO/setup.bash
 if [ $ROS_DISTRO == "melodic" ]; then
     printf "\n\nPlease ensure flycaptue is installed from this repo: https://github.com/Juched/flycap-mirror\n\n\n"
     read -p "Press enter to continue"
 else
-    . install_point_grey_drivers.sh
+    ./install_point_grey_drivers.sh
 fi
-. install_custom_ros_packages.sh
-sudo . install_ceres.sh
-sudo . install_eigen.sh
-. setup_uwrt_env.sh
+./install_custom_ros_packages.sh
+sudo ./install_ceres.sh
+sudo ./install_eigen.sh
+./setup_uwrt_env.sh
