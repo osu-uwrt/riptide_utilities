b6c5ed89e405db4f88bb98ca4c1750c1411b2f4c
Blaine Miller <blaine141@gmail.com>
2020-01-10 16:39:26 -0500

    better bashrc setup

--- setup_scripts/setup_bashrc.sh
+++ setup_scripts/setup_bashrc.sh
@@ -9,6 +9,7 @@ s3="source ~/osu-uwrt/riptide_software/devel/setup.bash"
 
 # Add appropriate lines to the bashrc if they do not exist
 if ! grep -q "$s1" ~/.bashrc || ! grep -q "$s2" ~/.bashrc || ! grep -q "$s3" ~/.bashrc; then
+    sed -i "/setup.bash/d" ~/.bashrc
     echo $s1 >> ~/.bashrc
     echo $s2 >> ~/.bashrc
     echo $s3 >> ~/.bashrc
